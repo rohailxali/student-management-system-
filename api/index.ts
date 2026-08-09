c72491117bece77877ac14d2a097a7a8b871ee18
rohailxali <mrohailali4@gmail.com>
2026-08-10 01:05:40 +0500

    fix: remove dotenv/config from api/index.ts to prevent ESM bundle crash on Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "../server/_core/oauth";
